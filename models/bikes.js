edc9a495848073f60319e7889881d4a0d3d751de
fawad-palletpal <93568319+fawad-palletpal@users.noreply.github.com>
2023-02-16 20:08:03 +0500

    add react app

--- models/bikes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Bikes = new mongoose.Schema({
-  make: String,
-  model: String,
-  isRented: Boolean,
-});
-
-module.exports = new mongoose.model("bikes", Bikes);
